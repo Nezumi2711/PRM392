8c458abe917bdccbefb2996f5304fba2ef31f269
phanhuuloi27 <73208452+Nezumi2711@users.noreply.github.com>
2022-07-20 16:41:53 +0700

    Fix font

--- Foodify/app/src/main/java/com/waterbase/foodify/Home.java
+++ Foodify/app/src/main/java/com/waterbase/foodify/Home.java
@@ -261,7 +261,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 .showNotification(false)
                 .showBgdToast(false)
                 .apkVersionName(Common.versionAppNewest + ".0 ")
-                .apkDescription(Common.changelog)
+                .apkDescription(String.format(Common.changelog))
                 .dialogProgressBarColor(0xFFFF5353)
                 .jumpInstallPage(false)
                 .build();
